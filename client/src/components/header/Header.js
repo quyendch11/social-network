4103e9980b55fcd138682cd3dcde4673fa3cffe5
sonph <sonph>
2022-09-19 12:58:26 +0700

    add ducan2

--- client/src/components/header/Header.js
+++ client/src/components/header/Header.js
@@ -11,7 +11,7 @@ const Header = () => {
             bg-light justify-content-between align-middle">
 
                 <Link to="/" className="logo">
-                    <h1 className="navbar-brand text-uppercase p-0 m-0"
+                    <h1 className="navbar-brand text-lowercase p-0 m-0"
                     onClick={() => window.scrollTo({top: 0})}>
                        INSTAOMLEM
                     </h1>
